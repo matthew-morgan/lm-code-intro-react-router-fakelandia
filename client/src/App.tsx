2bcfaaf553683ca5e8fb1289b68a7f7793b8c352
Matthew Morgan <matthew@hidlen.com>
2023-02-18 18:14:00 +0000

    Adds basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,18 @@ import Misdemeanour from './components/Misdemeanour';
 import Confession from './components/Confession';
 import Home from './components/Home';
 import NotFound from './components/NotFound';
+import Layout from './components/Layout';
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/misdemeanour" element={<Misdemeanour />} />
-      <Route path="/confession" element={<Confession />} />
-      <Route path='*' element={<NotFound />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/misdemeanour" element={<Misdemeanour />} />
+        <Route path="/confession" element={<Confession />} />
+        <Route path='*' element={<NotFound />} />
+      </Route>
     </Routes>
   );
 }
