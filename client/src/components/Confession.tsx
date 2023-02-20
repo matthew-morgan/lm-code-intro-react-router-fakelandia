937488b5b91bbeb438470a8f5793b8e190f0f5f7
Matthew Morgan <matthew@hidlen.com>
2023-02-20 20:23:10 +0000

    Adds SubjectInput subcomponent to Confession

--- client/src/components/Confession.tsx
+++ client/src/components/Confession.tsx
@@ -1,3 +1,24 @@
-const Confession : React.FC = () => <>Confession!</>;
+const Confession: React.FC = () => <>Confession!</>;
+
+type Props = {
+    value: string;
+    onChange: (value: string) => void;
+};
+
+const SubjectInput: React.FC<Props> = ({ value, onChange }) => {
+    return (
+        <div>
+            <label htmlFor="subject">Subject:</label>
+            <input
+                type="text"
+                id="subject"
+                value={value}
+                onChange={(e) => onChange(e.target.value)}
+            />
+        </div>
+    );
+};
+
+
 
 export default Confession;
