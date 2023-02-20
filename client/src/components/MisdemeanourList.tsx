7fc545e268e18fe31b7350ab9528bc41d3dfb2c8
Matthew Morgan <matthew@hidlen.com>
2023-02-20 16:54:52 +0000

    Adds a dropdown to filter misdemeanours

--- client/src/components/MisdemeanourList.tsx
+++ client/src/components/MisdemeanourList.tsx
@@ -1,6 +1,6 @@
 import { MisdemeanourContext } from "../App";
-import { useContext } from 'react';
-import { Misdemeanour } from "../types/misdemeanours.types";
+import { useContext, useState } from 'react';
+import { Misdemeanour, MISDEMEANOURS } from "../types/misdemeanours.types";
 
 const MisdemeanourList: React.FC = () => {
   const misdemeanours = useContext(MisdemeanourContext);
@@ -14,6 +14,14 @@ const MisdemeanourList: React.FC = () => {
 };
 
 const MisdemeanourTable: React.FC<{ misdemeanours: Misdemeanour[] }> = ({ misdemeanours }) => {
+  const [selectedMisdemeanour, setSelectedMisdemeanour] = useState<string>("All");
+
+  const filteredMisdemeanours = selectedMisdemeanour !== "All"
+    ? misdemeanours.filter(m => m.misdemeanour === selectedMisdemeanour)
+    : misdemeanours;
+
+  const misdemeanourOptions = ['All', ...MISDEMEANOURS];
+  
   return (
     <table>
       <thead>
@@ -22,9 +30,18 @@ const MisdemeanourTable: React.FC<{ misdemeanours: Misdemeanour[] }> = ({ misdem
           <th>Misdemeanour</th>
           <th>Date</th>
         </tr>
+        <tr>
+          <th></th>
+          <th>
+            <select value={selectedMisdemeanour} onChange={(e) => setSelectedMisdemeanour(e.target.value)}>
+              {misdemeanourOptions.map((option) => <option key={option} value={option}>{option}</option>)}
+            </select>
+          </th>
+          <th></th>
+        </tr>
       </thead>
       <tbody>
-        {misdemeanours.map(m => (
+        {filteredMisdemeanours.map(m => (
           <tr key={m.citizenId}>
             <td>{m.citizenId}</td>
             <td>{m.misdemeanour}</td>
@@ -35,5 +52,6 @@ const MisdemeanourTable: React.FC<{ misdemeanours: Misdemeanour[] }> = ({ misdem
     </table>
   );
 };
+
 
 export default MisdemeanourList;
