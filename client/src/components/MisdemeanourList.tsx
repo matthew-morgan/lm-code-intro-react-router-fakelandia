544a6771f3b62e5227a964df8974dd1554be1673
Matthew Morgan <matthew@hidlen.com>
2023-02-20 16:22:09 +0000

    Adds sub-component to render misdemeanours list

--- client/src/components/MisdemeanourList.tsx
+++ client/src/components/MisdemeanourList.tsx
@@ -1,18 +1,39 @@
 import { MisdemeanourContext } from "../App";
 import { useContext } from 'react';
+import { Misdemeanour } from "../types/misdemeanours.types";
 
 const MisdemeanourList: React.FC = () => {
-
   const misdemeanours = useContext(MisdemeanourContext);
 
   return (
     <div>
       <h2>Misdemeanours:</h2>
-      <ul>
-        {misdemeanours.map(m => <li key={m.citizenId}>{m.citizenId}</li>)}
-      </ul>
+      <MisdemeanourTable misdemeanours={misdemeanours} />
     </div>
   );
-}
+};
+
+const MisdemeanourTable: React.FC<{ misdemeanours: Misdemeanour[] }> = ({ misdemeanours }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Citizen ID</th>
+          <th>Misdemeanour</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {misdemeanours.map(m => (
+          <tr key={m.citizenId}>
+            <td>{m.citizenId}</td>
+            <td>{m.misdemeanour}</td>
+            <td>{m.date}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default MisdemeanourList;
