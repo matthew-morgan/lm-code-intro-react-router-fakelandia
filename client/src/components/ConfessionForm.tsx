15481941c314642a7cb00fdf3c2ee8de3411bd37
Matthew Morgan <matthew@hidlen.com>
2023-02-21 15:11:18 +0000

    POSTs form data to endpoint

--- client/src/components/ConfessionForm.tsx
+++ client/src/components/ConfessionForm.tsx
@@ -18,10 +18,51 @@ const ConfessionForm: React.FC = () => {
   });
   const [isFormValid, setIsFormValid] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Submit form data
+
+    // Clear any previous messages
+    setErrorMessage('');
+    setSuccessMessage('');
+
+    const { subject, reason, details } = formValues;
+    const formData = {
+      subject,
+      reason: reason === 'just-talk' ? reason : reason as MisdemeanourKind,
+      details,
+    };
+
+    try {
+      const response = await fetch('http://localhost:8080/api/confess', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (!data.success) {
+        setErrorMessage(data.message);
+      } else {
+        setSuccessMessage('Form submitted successfully!');
+      }
+
+      if (!response.ok) {
+        //throw new Error(`Failed to submit form: ${response.status}`);
+        setErrorMessage(`Failed to submit form: ${response.status}`);
+      }
+      // Handle the server response
+    } catch (error) {
+      // Handle network errors and failed requests
+      setErrorMessage("network error");
+    }
   };
+
 
   const handleInputChange = (name: string, value: string | MisdemeanourKind) => {
     setFormValues({ ...formValues, [name]: value });
@@ -37,23 +78,28 @@ const ConfessionForm: React.FC = () => {
 
 
   return (
-    <form onSubmit={handleSubmit}>
-      <SubjectInput
-        value={formValues.subject}
-        onChange={(value) => handleInputChange('subject', value)}
-      />
-      <ReasonDropdown
-        value={formValues.reason}
-        onChange={(value) => handleInputChange('reason', value)}
-      />
-      <DetailsTextarea
-        value={formValues.details}
-        onChange={(value) => handleInputChange('details', value)}
-      />
-      <button type="submit" disabled={!isFormValid}>
-        Submit
-      </button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <SubjectInput
+          value={formValues.subject}
+          onChange={(value) => handleInputChange('subject', value)}
+        />
+        <ReasonDropdown
+          value={formValues.reason}
+          onChange={(value) => handleInputChange('reason', value)}
+        />
+        <DetailsTextarea
+          value={formValues.details}
+          onChange={(value) => handleInputChange('details', value)}
+        />
+        <button type="submit" disabled={!isFormValid}>
+          Submit
+        </button>
+      </form>
+
+      {errorMessage && <p>{errorMessage}</p>}
+      {successMessage && <p>{successMessage}</p>}
+    </>
   );
 };
 
